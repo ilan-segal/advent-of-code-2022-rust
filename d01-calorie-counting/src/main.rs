ccce3d76d15988733a501e91610549007e9c1a6c
Ilan-Voyager <ilan.segal@mail.utoronto.ca>
2023-09-25 13:10:24 -0400

    Complete Day 1

--- d01-calorie-counting/src/main.rs
+++ d01-calorie-counting/src/main.rs
@@ -1,22 +1,27 @@
 use common;
 
-fn parse_items(items: &str) -> Vec<i32> {
+fn parse_items(items: &str) -> u32 {
     items
         .lines()
         .map(|item| {
-            item.parse::<i32>()
+            item.parse::<u32>()
                 .expect("Failed to parse item")
         })
-        .collect()
+        .sum()
 }
 
 fn main() {
-    let result = common::read_file("input.txt")
+    let mut totals: Vec<u32> = common::read_file("input.txt")
         .replace("\r\n", "\n")
         .split("\n\n")
         .map(parse_items)
-        .map(|items| items.iter().sum::<i32>())
-        .max()
-        .expect("Failed to find maximum sum of items");
-    println!("{}", result)
+        .collect();
+    totals.sort();
+    println!(
+        "{}",
+        totals
+            .last()
+            .expect("Failed to get maximum element")
+    );
+    println!("{}", totals.iter().rev().take(3).sum::<u32>());
 }
